884bfdebbf841786822a88963bc75a7d8f608fd0
Ragip Gjinovci <ragip.gjinovci@probit-kos.com>
2020-04-12 07:21:08 +0200

    endi

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 // const swup = new Swup()
+const menu = document.querySelector('.toggled-menu');
+const menuButton = document.querySelector('#toggle-menu');
+menuButton.addEventListener('click', function(){
+  menu.style.display = 'block';
+})
